ab116261aadad7c357d7578846f47f4223d221a2
Nathan Friedly <nathan@nfriedly.com>
2017-04-30 15:59:00 -0400

    reactify'ing ads

--- components/BannerAdd.js
+++ components/BannerAdd.js
@@ -1,16 +1,9 @@
+import GoogleAd from 'react-google-ad'
+
 export default () => (
-  <div className="banner-add">
-    <style jsx>{`
-      .banner-add { text-align: center; }
-      .adsbygoogle { display:inline-block;width:728px;height:90px }
-    `}
-    </style>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins className="adsbygoogle"
-         data-ad-client="ca-pub-9477050254721722"
-         data-ad-slot="0871459249"></ins>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+  <div style={{textAlign: 'center'}}>
+    {/* format="xxx" */}
+    <GoogleAd client='ca-pub-9477050254721722'
+              slot='0871459249' />
   </div>
 )
