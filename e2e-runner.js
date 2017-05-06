306791cdaa4917cb695d7100c3a0d833d80804eb
Nathan Friedly <nathan@nfriedly.com>
2017-05-05 20:21:54 -0500

    handle errors in e2e runner

--- e2e-runner.js
+++ e2e-runner.js
@@ -3,6 +3,16 @@ const cp = require('child_process');
 const app = cp.spawn('npm', ['start'], { stdio: 'inherit' });
 const test = cp.spawn('nightwatch', [], { stdio: 'inherit' });
 
+const handleErr = err => {
+  console.error(err);
+  app.kill();
+  test.kill();
+  process.exit(1);
+};
+
+app.on('error', handleErr);
+test.on('error', handleErr);
+
 test.on('exit', code => {
   app.kill();
   app.on('exit', () => {
